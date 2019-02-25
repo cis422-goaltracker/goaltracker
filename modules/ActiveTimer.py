952d8437513da7dc848b27f4f2d50852a1df6aae
Noah Palmer <noahjkpalmer@gmail.com>
2019-02-24 21:27:19 -0800

    ActiveTimer complete, needs to be tested

--- modules/ActiveTimer.py
+++ modules/ActiveTimer.py
@@ -13,15 +13,12 @@ class ActiveTimer(object):
 		pass
 
 	"""METHODS FOR ACTIVETIMER"""
-	def calculateActiveTime(self, _gid, _model, _currentDate): #FUNCTION NEEDS TO BE BUILT
+	def calculateActiveTime(self, _gid, _model, _currentDate):
 		goal = _model.getGoal(_gid) #gets the goal form the model
-
-		elapsedTime = None
-		#if complete, goal finishdate - startdate
-
-		#if not complete, currentdate - startdate
-
-		return elapsedTime #returns the elapsed time
+		if goal.isComplete(): #if goal is complete
+			return goal.getFinishDate() - goal.getStartDate() #find the difference between finish and start date
+		else:
+			return _currentDate - goal.getStartDate() #find the difference between the current date and start date
 
 	"""GETTERS FOR ACTIVETIMER"""
 
